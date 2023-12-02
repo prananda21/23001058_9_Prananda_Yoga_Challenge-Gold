ac0ae79ccb0c806452961b1f3d8013fc55a28b17
Prananda Yoga <pranandayoga21@gmail.com>
2023-12-02 17:11:41 +0800

    wrote some code in index.js contain starting server using express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// root => halamam utama
+app.get('/', (req, res) => {
+    res.send('Halo gais, ini submission Challenge Gold dari Prananda Yoga');
+})
+
+app.get('/about', (req, res) => {
+    res.send('Ini adalah halaman about');
+})
+
+app.listen(port, () => {
+    console.log(`This app listening at http://localhost:${port}`);
+})
